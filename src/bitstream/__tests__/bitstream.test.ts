3b6b13f132b751436ef39a37a9deb4cc75347d22
Blayne Chard <blayne@chard.com>
2020-08-29 15:56:40 +1200

    feat(bitstream): bitstream string reader
    Will read null terminated or fixed length strings from a bit stream

---
+++ src/bitstream/__tests__/bitstream.test.ts
@@ -0,0 +1,32 @@
+import o from 'ospec';
+import { BitStream } from '..';
+
+o.spec('BitStream', () => {
+  function toNullString(str: string): Buffer {
+    const buf = [];
+    for (let i = 0; i < str.length; i++) buf.push(str.charCodeAt(i));
+    return Buffer.from(buf);
+  }
+  o('should read bits from a stream', () => {
+    const reader = new BitStream([0b1]);
+    o(reader.bit()).equals(1);
+    o(reader.offset).equals(1);
+    o(reader.maxOffset).equals(8);
+    o(reader.bit()).equals(0);
+  });
+
+  o('should read little endian', () => {
+    o(new BitStream([0xff]).bits(8)).equals(255);
+    o(new BitStream([0xff, 0xff]).bits(16)).equals(256 * 256 - 1);
+    o(new BitStream([0xff, 0xff, 0xff, 0xff]).bits(32)).equals(256 ** 4 - 1);
+  });
+
+  o('should read a string', () => {
+    // o(new BitStream(toNullString('Hello')).string()).equals('Hello');
+    // o(new BitStream(toNullString('Hello World')).string(2)).equals('He');
+
+    const reader = new BitStream(toNullString('Hello\x00World'));
+    o(reader.string()).equals('Hello');
+    o(reader.string()).equals('World');
+  });
+});
--- src/bitstream/index.ts
+++ src/bitstream/index.ts
@@ -9,12 +9,16 @@ for (let i = 0; i < 64; i++) Power2[i] = Math.pow(2, i);
 
 /** Very basic bit reader */
 export class BitStream {
+  /** Number of bits through the stream */
   offset: number;
+  /** Raw buffer */
   buffer: StrutParserInput;
+  /** Max bits that can be read */
   maxOffset: number;
+  /** How to read multiple bits at a time */
   isLittleEndian: boolean;
 
-  constructor(buffer: StrutParserInput, offset: number, maxOffset: number, isLittleEndian = true) {
+  constructor(buffer: StrutParserInput, offset = 0, maxOffset: number = buffer.length, isLittleEndian = true) {
     this.buffer = buffer;
     this.offset = offset * 8;
     this.maxOffset = maxOffset * 8;
@@ -31,23 +35,51 @@ export class BitStream {
     return (byte & (((1 << (offset + length)) - 1) & ~((1 << offset) - 1))) >> offset;
   }
 
+  /**
+   * Skip number of bits
+   * @param bits number of bits to skip
+   */
+  skip(bits: number): void {
+    this.offset += bits;
+  }
+
   bit(): number {
+    if (this.remainingBits < 0) throw new Error('BitStream: Overflow ' + this.remainingBits);
+
     const bytePos = Math.floor(this.offset / 8);
     const bitPos = this.offset % 8;
     const byte = this.buffer[bytePos];
-    if (this.remainingBits < 0) throw new Error('BitStream: Overflow ' + this.remainingBits);
     this.offset++;
     return this.getBitValue(byte, bitPos, 1);
   }
 
-  /** Read bits little endian */
+  /** Read bits */
   bits(length: number): number {
     const initialLen = length;
     let bits = 0;
     while (length > 0) {
-      bits += (this.bit() ? 1 : 0) * Power2[initialLen - length];
-      length -= 1;
+      const bit = this.bit();
+      if (bit == 1) bits += Power2[initialLen - length];
+      length--;
     }
     return bits;
+  }
+
+  /**
+   * Read a string up to length
+   *
+   * Will break early if a null character is found
+   *
+   * @param length max length to read, defaults to entire buffer
+   */
+  string(length?: number): string {
+    const buf = [];
+    if (length == null) length = Math.floor(this.remainingBits / 8);
+    for (let i = 0; i < length; i++) {
+      const chr = this.bits(8);
+      if (chr == 0x00) break;
+      buf.push(String.fromCharCode(chr));
+    }
+    return buf.join('');
   }
 }
